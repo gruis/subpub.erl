d6df4dde5aadd8e00cc029fd6b5bb1376bddd028
Caleb Crane <github@simulacre.org>
2012-03-04 22:05:09 +0900

    support unsubscribe and quit

--- sender.rb
+++ sender.rb
@@ -1,18 +1,21 @@
 #!/usr/bin/env ruby -wW1
 
 require 'socket'
-s = TCPSocket.open("127.0.0.1", "8888")
+s = TCPSocket.open("127.0.0.1", "6389")
 s.write("*3\r\n$7\r\nPUBLISH\r\n$6\r\nquotes\r\n$75\r\nWhen Chuck Norris wants a steak, cows volunteer. It's just easier that way.\r\n")
 puts s.gets.inspect
 
 s.write("SUBSCRIBE quotes scores\r\n")
 puts s.readpartial(1024).inspect
-
-s.write("SUBSCRIBE movies episodes\r\n")
+puts s.readpartial(1024).inspect
+#s.write("UNSUBSCRIBE quotes scores\r\n")
 puts s.readpartial(1024).inspect
 
+#s.write("SUBSCRIBE movies episodes\r\n")
+#puts s.readpartial(1024).inspect
+
 #s.write("SHUTDOWN\r\n")
-puts s.gets.inspect
-puts s.gets.inspect
-puts s.gets.inspect
-puts s.gets.inspect
+puts s.readpartial(1024).inspect
+puts s.readpartial(1024).inspect
+puts s.readpartial(1024).inspect
+puts s.readpartial(1024).inspect
--- src/subpub.erl
+++ src/subpub.erl
@@ -4,7 +4,7 @@
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
 listen() ->
-  listen(8888).
+  listen(6389).
 
 listen(Port) ->
   {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
@@ -36,18 +36,15 @@ handle_client(Client) ->
 loop_client(Client) ->
     case gen_tcp:recv(Client, 0, 100) of
         {ok, Data} ->
-          io:format("recv: ~p.~n", [Data]),
           [{command, Command} | Rest] = redis_cmd_parser:parse(binary_to_list(Data)),
           Reply = procmd(Client, Command, Rest),
-          io:format("resp: ~p.~n", [Reply]),
           tell_client(Client, Reply),
           loop_client(Client);
         {error, timeout} ->
           receive
             {publish, Topic, Msg} -> 
               case lists:any(fun(E) -> E == Topic end, get(subs)) of
-                  true ->
-                      tell_client(Client, Msg);
+                  true -> tell_client(Client, publish(Client, Topic, Msg));
                   _ -> nothing
               end,
               loop_client(Client)
@@ -63,24 +60,32 @@ loop_client(Client) ->
 
 procmd(_Client, "PUBLISH", [{topic, Topic}, {msg, Msg}]) ->
   broadcaster ! {publish, Topic, Msg},
-  "ok";
+  ":0";
 
 procmd(Client, "SUBSCRIBE", [{topics, Topics}]) ->
-  io:format("SUBSCRIBE ~p.~n", [Topics]),
   lists:foldl(
     fun(Topic, Reply) -> string:concat(Reply, subscribe(Client, Topic)) end, 
     "", 
     Topics);
 
+procmd(Client, "UNSUBSCRIBE", [{topics, Topics}]) ->
+  lists:foldl(
+    fun(Topic, Reply) -> string:concat(Reply, unsubscribe(Client, Topic)) end, 
+    "", 
+    Topics);
+
+procmd(Client, "QUIT", _) ->
+  tell_client(Client, "+ok"),
+  exit("QUIT");
+
 procmd(Client, "SHUTDOWN", _) ->
-  tell_client(Client, "ok"),
+  tell_client(Client, "+ok"),
   halt();
 
 procmd(_Client, Command, _Args) ->
   string:concat("unrecognized command ", Command).
 
 subscribe(_Client, Topic) ->
-  io:format("subscribe ~p.~n", [Topic]),
   put(subs, [Topic|get(subs)]),
   Cnt = length(get(subs)),
   lists:foldl(fun(E, Sum) -> string:concat(Sum, E) end, "*3\r\n$9\r\nsubscribe\r\n$", 
@@ -89,6 +94,31 @@ subscribe(_Client, Topic) ->
       Topic,
       "\r\n:",
       integer_to_list(Cnt),
+      "\r\n"
+    ]).
+
+unsubscribe(_Client, Topic) ->
+  Subs = get(subs),
+  put(subs, [E || E <- Subs, E =/= Topic]),
+  Cnt = length(get(subs)),
+  lists:foldl(fun(E, Sum) -> string:concat(Sum, E) end, "*3\r\n$11\r\nunsubscribe\r\n$", 
+    [ integer_to_list(length(Topic)),
+      "\r\n",
+      Topic,
+      "\r\n:",
+      integer_to_list(Cnt),
+      "\r\n"
+    ]).
+
+publish(_Client, Topic, Msg) ->
+  lists:foldl(fun(E, Sum) -> string:concat(Sum, E) end, "*3\r\n$7\r\nmessage\r\n$", 
+    [ integer_to_list(length(Topic)),
+      "\r\n",
+      Topic,
+      "\r\n$",
+      integer_to_list(length(Msg)),
+      "\r\n",
+      Msg,
       "\r\n"
     ]).
 
